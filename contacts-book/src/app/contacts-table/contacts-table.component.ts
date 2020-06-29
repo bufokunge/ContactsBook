a9f70cc5ab2ac49194e4fbbe1f36baea655585be
Katharina Unger <>
2020-06-29 11:32:03 +0200

    Implement form to add contact

--- contacts-book/src/app/contacts-table/contacts-table.component.ts
+++ contacts-book/src/app/contacts-table/contacts-table.component.ts
@@ -3,7 +3,6 @@ import { Observable } from "rxjs";
 import { Contact, SimplifiedContact } from "../contact";
 import { select, Store } from "@ngrx/store";
 import { AddContact } from "../contact.action";
-import { mockContactFactory } from "../contact.mocks";
 import { MatDialog } from "@angular/material/dialog";
 import { CreateContactComponent } from "../create-contact/create-contact.component";
 
@@ -31,23 +30,13 @@ export class ContactsTableComponent implements OnInit {
     const dialogRef = this.dialog.open(CreateContactComponent, {width: '250px', data: contact});
 
     dialogRef.afterClosed().subscribe(data => {
-      console.log('Dialog was closed');
-      console.log('data', data);
       this.addContact(data);
     })
   }
 
   addContact(contact: Contact) {
-    // TODO: add contact passed as parameter
-    console.log(contact);
+    if (!contact) return;
 
-    const mockContactPromise = mockContactFactory(1);
-
-    mockContactPromise.then(data => {
-      const mockContact = data['contacts'][0]
-      console.log('add contact', mockContact);
-
-      this.store.dispatch(new AddContact(mockContact));
-    });
+    this.store.dispatch(new AddContact(contact));
   }
 }
--- contacts-book/src/app/create-contact/create-contact.component.ts
+++ contacts-book/src/app/create-contact/create-contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import { Contact } from "../contact";
+import { mockContactFactory } from "../contact.mocks";
 
 @Component({
   selector: 'app-create-contact',
@@ -13,6 +14,18 @@ export class CreateContactComponent implements OnInit {
               @Inject(MAT_DIALOG_DATA) public contact: Contact) { }
 
   ngOnInit(): void {
+  }
+
+  onSetMockData(): void {
+    const mockContactPromise = mockContactFactory(1);
+
+    mockContactPromise.then(data => {
+      this.contact = data['contacts'][0];
+    });
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
   }
 
 
