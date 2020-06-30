ba3b793848f56d48227d51b838b10c8e401fd8b4
Katharina Unger <>
2020-06-30 12:04:50 +0200

    Add test to check rows in table

--- contacts-book/src/app/contacts-table/contacts-table.component.spec.ts
+++ contacts-book/src/app/contacts-table/contacts-table.component.spec.ts
@@ -8,6 +8,7 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterTestingModule } from "@angular/router/testing";
 import { Router } from "@angular/router";
+import { mockContactFactory } from "../contact.mocks";
 
 describe('ContactsTableComponent', () => {
   let component: ContactsTableComponent;
@@ -58,4 +59,23 @@ describe('ContactsTableComponent', () => {
     expect(url).toBe('contact/' + ssn);
   }));
 
+  it('should have as many rows as contacts or paginator page size', () => {
+    const pageSize = component.paginator.pageSize;
+    const mockContactPromise = mockContactFactory(pageSize + 1);
+
+    return mockContactPromise.then(data => {
+      component.dataSource.data = data['contacts'];
+      fixture.detectChanges();
+      let rowsNr = fixture.debugElement.nativeElement.querySelector('tbody').children.length;
+
+      expect(rowsNr).toBe(pageSize);
+
+      component.dataSource.data = data['contacts'].slice(0, pageSize - 1);
+      fixture.detectChanges();
+      rowsNr = fixture.debugElement.nativeElement.querySelector('tbody').children.length;
+
+      expect(rowsNr).toBe(pageSize - 1);
+    });
+
+  });
 });
--- contacts-book/src/app/services/data.service.spec.ts
+++ contacts-book/src/app/services/data.service.spec.ts
@@ -3,24 +3,44 @@ import { TestBed } from '@angular/core/testing';
 import { DataService } from './data.service';
 import { provideMockStore } from "@ngrx/store/testing";
 import { SimplifiedContact } from "../contact";
+import { mockContactFactory } from "../contact.mocks";
 
 describe('DataService', () => {
   let service: DataService;
-  const initialState: {[ssn: number]: SimplifiedContact} = {};
+  const initialState: { [ssn: number]: SimplifiedContact } = {};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         provideMockStore({initialState}),
       ],
-        imports: []
+      imports: []
     }).compileComponents();
-    service = TestBed.inject(DataService);
 
+    service = TestBed.inject(DataService);
+    service.contacts = {};
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return contact by ssn', () => {
+    const mockContactPromise = mockContactFactory(3);
+
+    return mockContactPromise.then(data => {
+      const mockContacts = data['contacts'];
+
+      let contact = service.getContactBySsn(data['contacts'][0].ssn);
+      expect(contact).toBeUndefined();
+
+      mockContacts.forEach(contact => {
+        service.contacts[contact.ssn] = contact;
+      });
+
+      contact = service.getContactBySsn(data['contacts'][0].ssn);
+      expect(contact).toBe(data['contacts'][0]);
+    });
   });
 
 });
